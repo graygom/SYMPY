0ac1d658e743dabe8fb05922991009a02d2a23ef
Hyunseung Yoo <hyunseung.yoo.hs@gmail.com>
2025-05-29 06:36:48 +0900

    Update radius_of_curvature.py

--- radius_of_curvature.py
+++ radius_of_curvature.py
@@ -1,9 +1,10 @@
 #
-# TITLE: Radius of Curvature
+# TITLE: SymPy
 # AUTHOR: Hyunseung Yoo
 # PURPOSE:
 # REVISION:
-# REFERENCE: https://www.youtube.com/playlist?list=PLJtAfFg1nIX9dsWGnbgFt2dqvwXVRSVxm
+# REFERENCE: https://docs.sympy.org/latest/modules/plotting.html
+#
 #
 
 
@@ -21,11 +22,17 @@ r_minor = sp.symbols('r_mi')
 x_pos = r_major * sp.cos(theta)
 y_pos = r_minor * sp.sin(theta)
 
+print(x_pos)
+print(y_pos)
+
 #
 dx_dtheta = x_pos.diff(theta)
 dy_dtheta = y_pos.diff(theta)
 d2x_dtheta2 = x_pos.diff(theta, 2)
 d2y_dtheta2 = y_pos.diff(theta, 2)
+
+print(dx_dtheta)
+print(d2x_dtheta2)
 
 #
 R  = (dy_dtheta**2 + dx_dtheta**2)**1.5
@@ -33,14 +40,24 @@ R /=  ((dx_dtheta*d2y_dtheta2 - dy_dtheta*d2x_dtheta2)**2)**0.5
 
 vals = {}
 vals[r_major] = 5.0
-vals[r_minor] = 5.0
+vals[r_minor] = 4.0
 
 R_try = R.subs(vals).simplify()
 
-# debugging
-print(x_pos)
-print(y_pos)
-print(dx_dtheta)
-print(d2x_dtheta2)
 print(R)
 print(R_try)
+
+# plot parametric
+vals = {}
+vals[r_major] = 5.0
+vals[r_minor] = 4.0
+
+plot_x = x_pos.subs(vals).simplify()
+plot_y = y_pos.subs(vals).simplify()
+plot_R = R_try = R.subs(vals).simplify()
+
+plot_xy = (plot_x, plot_y)
+plot_thetaR = (theta, plot_R)
+
+sp.plot_parametric(plot_thetaR, (theta, 0.0, 2*sp.pi))
+
